f6786a5a31cfce7aaac06572b13e0e500a1360c7
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-22 15:02:14 -0400

    fix: corregir anotaciones de autorización en TestController y limpiar el código

--- demos/api-gestion-academica/src/main/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/restcontoller/TestController.java
+++ demos/api-gestion-academica/src/main/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/restcontoller/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/test")
 public class TestController {
-	
+
     @GetMapping("/libre/demo")
     public String allAccess() {
         return "Public content";
@@ -19,30 +19,31 @@ public class TestController {
     public String endpoint1() {
         return "User board- admin";
     }
-    
+
     @GetMapping("/private/endpoint2")
     @PreAuthorize("hasAuthority('director_Academico')")
     public String endpoint3() {
         return "User board - director";
     }
-    
+
     @GetMapping("/private/endpoint3")
     @PreAuthorize("hasAnyAuthority('profesor')")
     public String endpoint4() {
         return "User board - profesor";
     }
-    
-   
+
     @GetMapping("/private/endpoint5")
-    @PreAuthorize("hasRole('DEMO')") //ROLE_DEMO -> Keycloak
+    @PreAuthorize("hasAuthority('DEMO')")
     public String endpoint5() {
         return "User board";
     }
 
     /*
-    @GetMapping("/private/endpoint2")
-    @PreAuthorize("hasRole('administrator')")
-    public String endpoint2() {
-        return "Administrator board";
-    }*/
+     * @GetMapping("/private/endpoint2")
+     * 
+     * @PreAuthorize("hasRole('administrator')")
+     * public String endpoint2() {
+     * return "Administrator board";
+     * }
+     */
 }
