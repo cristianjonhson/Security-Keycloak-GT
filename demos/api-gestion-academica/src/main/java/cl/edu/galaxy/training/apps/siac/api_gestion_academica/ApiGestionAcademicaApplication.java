f2eecda7162a3146f70662ec47668728b5e87402
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-22 15:02:28 -0400

    fix: eliminar configuración de exclusión de DataSource en ApiGestionAcademicaApplication

--- demos/api-gestion-academica/src/main/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/ApiGestionAcademicaApplication.java
+++ demos/api-gestion-academica/src/main/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/ApiGestionAcademicaApplication.java
@@ -2,9 +2,8 @@ package cl.edu.galaxy.training.apps.siac.api_gestion_academica;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication/*(exclude = {DataSourceAutoConfiguration.class})*/
+@SpringBootApplication
 public class ApiGestionAcademicaApplication {
 
 	public static void main(String[] args) {
