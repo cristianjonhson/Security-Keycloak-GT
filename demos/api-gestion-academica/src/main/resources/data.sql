f3fa55acf3341b2558521bdf014db3b9fa634f89
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-04-24 12:13:16 -0400

    fix: agregar manejo de conflictos en inserciones de datos en data.sql

--- demos/api-gestion-academica/src/main/resources/data.sql
+++ demos/api-gestion-academica/src/main/resources/data.sql
@@ -4,11 +4,11 @@ values (
         'Centro comercial Moctezuma',
         'Francisco Chang',
         'Mexico'
-    );
+    ) ON CONFLICT (company_id) DO NOTHING;
 insert into tbl_company(company_id, company, contact, country)
 values (
         2,
         'Alfreds Futterkiste',
         'Maria Anders',
         'Germany'
-    );
+    ) ON CONFLICT (company_id) DO NOTHING;
