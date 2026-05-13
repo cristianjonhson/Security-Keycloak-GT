00651d5402f88f35e116bc4de70cee647b4e3d85
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-13 11:51:45 -0400

    fix: agregar propiedades de configuración para pruebas en ApiGestionAcademicaApplicationTests

--- demos/api-gestion-academica/src/test/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/ApiGestionAcademicaApplicationTests.java
+++ demos/api-gestion-academica/src/test/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/ApiGestionAcademicaApplicationTests.java
@@ -2,8 +2,17 @@ package cl.edu.galaxy.training.apps.siac.api_gestion_academica;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:postgresql://localhost:5432/db_academica",
+		"spring.datasource.username=postgres",
+		"spring.datasource.password=postgres",
+		"spring.datasource.driver-class-name=org.postgresql.Driver",
+		"keycloak.resource=api-gestion-academica",
+		"spring.security.oauth2.resourceserver.jwt.jwk-set-uri=http://localhost:6082/realms/REALM_DEMO_V1/protocol/openid-connect/certs"
+})
+@ActiveProfiles("test")
 class ApiGestionAcademicaApplicationTests {
 
 	@Test
