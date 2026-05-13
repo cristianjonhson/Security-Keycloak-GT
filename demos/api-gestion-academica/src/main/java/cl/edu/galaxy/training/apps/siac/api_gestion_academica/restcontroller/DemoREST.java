58e831fee6d266f2dcec1df4070687cdfa1c40a2
Cristian Jonhson Alvarez <cristian.jonhson@inacapmail.cl>
2026-05-13 11:23:17 -0400

    fix: eliminar método demov9 y su importación innecesaria en DemoREST

--- demos/api-gestion-academica/src/main/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/restcontroller/DemoREST.java
+++ demos/api-gestion-academica/src/main/java/cl/edu/galaxy/training/apps/siac/api_gestion_academica/restcontroller/DemoREST.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -130,13 +129,6 @@ public class DemoREST {
 	public ResponseEntity<List<String>> demov8(@RequestParam("contries") List<String> contries) {
 
 		return ResponseEntity.ok(contries);
-
-	}
-
-	@GetMapping("/v9")
-	public ResponseEntity<String> demov9(@RequestHeader("token") String token) {
-
-		return ResponseEntity.ok(token);
 
 	}
 
